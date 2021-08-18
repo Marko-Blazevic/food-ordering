5107a1ce0f577d516f4155831c12fd51491550ed
Marko Blazevic <www.markoblazevic@gmail.com>
2021-08-18 19:05:14 -0300

    Added Amount

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -4,9 +4,18 @@ import classes from './MealItem.module.css';
 const MealItem = (props) => {
   return (
     <li className={classes['meal-item']}>
-      <h3>{props.name}</h3>
-      <p className={classes['meal-description']}>{props.description}</p>
-      <p className={classes['meal-price']}>${props.price}</p>
+      <div>
+        <h3>{props.name}</h3>
+        <p className={classes.description}>{props.description}</p>
+        <p className={classes.price}>${props.price}</p>
+      </div>
+      <div className={classes.amount}>
+        <div className={classes['amount-input']}>
+          <label htmlFor='amount'>Amount</label>
+          <input type='number' id='amount' min={0}></input>
+        </div>
+        <button>+ Add</button>
+      </div>
     </li>
   );
 };
